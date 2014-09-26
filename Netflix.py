65647fa4812477cc7b152c1d4fd0a04ea4afde25
jjloftin <johnloftin99@gmail.com>
2014-09-26 18:09:05 -0500

    Implement Prediction Algorithm. RMSE = 0.99

--- Netflix.py
+++ Netflix.py
@@ -4,16 +4,24 @@ import json
 #----------------
 # netflix_predict
 #----------------
-def netflix_predict(m_id,c_id):
+def netflix_predict(m_avg, c_avg):
   '''
   c_id - customer id
   m_id - movie id
+  p = prediction
   '''
-  base = 3.7
   
+  '''
+  Give a base rating. Calculate difference of movie and customer averages from the base. Weight them and add to base rating.
+  '''
+  base = 3.6
+  m_diff = m_avg - base
+  c_diff = c_avg - base
   
+  p = base + 0.7*m_diff + 0.3*c_diff
+ 
   
-  return 3.65
+  return p
 #-------------
 # netflix_rsme
 #-------------
@@ -34,6 +42,7 @@ def netflix_rmse(p):
     probe_dict[s[0],s[1]] = eval(s[2])
 
   
+  probe_data.close()
   '''
   Calulate and return the RMSE
   '''
@@ -43,11 +52,6 @@ def netflix_rmse(p):
   
   
   return (sum/len(p))**0.5
-#-------------
-# netflix_read
-#-------------
-def netflix_read(r):
-   return r.readline().strip()
 
 #-------------
 # netlix_print
@@ -72,6 +76,25 @@ def netflix_solve(r, w):
   w a writer
   '''
  
+  c_avgs = open('zwf69-TCustRatings.txt','r') 
+  c_avg_dict = {}
+  
+  i = 0
+  
+  for line in c_avgs:
+    s = line.strip().split(',')
+    c_avg_dict[s[0]] = eval(s[1])
+
+  c_avgs.close()
+   
+  m_avgs = open('sp35972-MovieAvg.txt','r')
+  m_avg_dict = {}
+  
+  for line in m_avgs:
+    s = line.strip().split(':')
+    m_avg_dict[s[0]] = eval(s[1])
+  
+  
   prediction_dict = {}
   
   c_id = 0
@@ -91,14 +114,14 @@ def netflix_solve(r, w):
     else:
       c_id = s
     
-    p = netflix_predict(m_id, c_id)
+    p = netflix_predict(m_avg_dict[m_id], c_avg_dict[c_id])
    
     prediction_dict[m_id,c_id] = p 
     
     w.write(str(p) + '\n')
     
   
-  rmse = netflix_rmse(prediction_dict)
+  rmse = int(netflix_rmse(prediction_dict) * 100) / 100
   w.write(str(rmse))  
     
     
